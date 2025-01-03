65fdb0934388056b4fc704ba722653c1b86d6720
paul.jeoung <kuni5458@naver.com>
2025-01-03 09:05:36 +0900

    FEATURE/CART - cart file reupload

---
+++ intelliJ/hexa-qna/src/main/java/com/example/hexaqna/service/CartService.java
@@ -0,0 +1,15 @@
+package com.example.hexaqna.service;
+
+import com.example.hexaqna.dto.CartDTO;
+
+import java.util.List;
+
+public interface CartService {
+    public List<CartDTO> addOrModify(CartDTO cartReqeustDTO);
+
+    public List<CartDTO> getCartItems(Long memberId);
+
+    //아이템 삭제
+    public List<CartDTO> remove(int cartId);
+}
+
