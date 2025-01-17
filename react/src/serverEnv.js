9f9dea1e2949b2460bfb916121b77ae58f7549bd
Paul v2 <114468411+PaulJeoung@users.noreply.github.com>
2025-01-17 11:53:22 +0900

    Update serverEnv.js

--- react/src/serverEnv.js
+++ react/src/serverEnv.js
@@ -1,2 +1,3 @@
-export const API_SERVER_HOST = "http://localhost:8010"
-export const REDIRECT_HOST = "http://localhost:3010"
+export const API_SERVER_HOST = process.env.REACT_APP_API_SERVER_HOST || "http://localhost:8010";
+export const REDIRECT_HOST = process.env.REACT_APP_REDIRECT_HOST || "http://localhost:3010";
+export const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
